039abbd3e51a94c7f4ab415a3ce9f48a8dc9b2db
kabipriya-mediwave <kabipriya@mindwaveventures.com>
2023-10-05 10:08:44 +0530

    added function

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,31 +1,3 @@
-// import "./App.css";
-// import Components from "./Components";
-
-// function App() {
-//   const { Button, Input } = Components;
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <Input
-//           name="First Name"
-//           type="text"
-//           placeholder="Enter your First name"
-//         />
-//         <Input
-//           name="Second Name"
-//           type="text"
-//           placeholder="Enter your Second name"
-//         />
-//         <Input name="Your Age" type="number" placeholder="Enter your age" />
-
-//         <Button />
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { Component } from "react";
 import "./App.css";
 import Components from "./Components";
